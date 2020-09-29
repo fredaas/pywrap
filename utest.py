c73bed6264d35be805308b902f10d377f43bc34b
Fredrik Ås <fredaas86@gmail.com>
2020-09-29 13:16:23 +0200

    Add new test and rename tests

--- utest.py
+++ utest.py
@@ -14,26 +14,33 @@ def read(path):
     return string
 
 # Test 1
-string = read("./tests/test_1.txt")
+string = read("./tests/test1.txt")
 string = wrap_block(string)
-write("./testout/test_1.txt", string)
+write("./testout/test1.txt", string)
 
 # Test 2
-string = read("./tests/test_2.txt")
+string = read("./tests/test2.txt")
 newlines, blocks = create_blocks(string)
-print(newlines, blocks)
 string = "\n" * newlines
 i = blocks[0][0]
 for _, n, b in blocks:
     string += wrap_block(b, i, n)
-write("./testout/test_2.txt", string)
+write("./testout/test2.txt", string)
 
 # Test 3
-string = read("./tests/test_3.txt")
+string = read("./tests/test3.txt")
 newlines, blocks = create_blocks(string)
-print(newlines, blocks)
 string = "\n" * newlines
 i = blocks[0][0]
 for _, n, b in blocks:
     string += wrap_block(b, i, n)
-write("./testout/test_3.txt", string)
+write("./testout/test3.txt", string)
+
+# Test 4
+string = read("./tests/test4.txt")
+newlines, blocks = create_blocks(string)
+string = "\n" * newlines
+i = blocks[0][0]
+for _, n, b in blocks:
+    string += wrap_block(b, i, n)
+write("./testout/test4.txt", string)
